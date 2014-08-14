1b298bb1bcb782d4f6f39e0389d2b7bc84241786
nikeah <redspidermkv@hotmail.com>
2014-08-14 22:28:04 +0100

    Missing form close call in ok button press

--- BasicGitClient/CredentialConfigureWindow.cs
+++ BasicGitClient/CredentialConfigureWindow.cs
@@ -30,6 +30,8 @@ namespace BasicGitClient
 
             Username = tbUsername.Text;
             Password = tbPassword.Text;
+
+            this.Close();
         }
 
         private void btnCancel_Click(object sender, EventArgs e)

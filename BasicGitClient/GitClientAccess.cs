cf2495f5bbe81be9e6865df20804d5f655528260
nikeah <redspidermkv@hotmail.com>
2014-08-20 21:45:56 +0100

    Comments added to git client access class

--- BasicGitClient/GitClientAccess.cs
+++ BasicGitClient/GitClientAccess.cs
@@ -7,13 +7,34 @@ using System.Text;
 
 namespace BasicGitClient
 {
+    /// <summary>
+    /// Class to access the git client via input/output redirection.
+    /// </summary>
     public class GitClientAccess
     {
+        #region Public Properties
+        
+        /// <summary>
+        /// Directory in which client will operate.
+        /// i.e. repo directory.
+        /// </summary>
         public string Directory { get; private set; }
 
+        #endregion
+
+        #region Private Data
+
+        // Process runner information.
         private ProcessStartInfo info;
         private Process gitProc;
 
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initialise a new object to access the git client.
+        /// </summary>
         public GitClientAccess()
         {
             info = new ProcessStartInfo
@@ -27,11 +48,25 @@ namespace BasicGitClient
             };
         }
 
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Change working directory.
+        /// </summary>
+        /// <param name="directory"></param>
         public void SetDirectory(string directory)
         {
             Directory = info.WorkingDirectory = directory;
         }
 
+        /// <summary>
+        /// Run a git command and retrieve the output and error messages.
+        /// </summary>
+        /// <param name="command">Git command to execute.</param>
+        /// <param name="stdout">Standard output from git process.</param>
+        /// <param name="stderror">Standard error from git process.</param>
         public void RunGitCommand(string command, out string stdout, out string stderror)
         {
             gitProc = new Process();
@@ -46,5 +81,7 @@ namespace BasicGitClient
             gitProc.WaitForExit();
             gitProc.Close();
         }
+
+        #endregion
     }
 }

687b7987310cf4d035f498a05bf6581bae031463
nikeah <redspidermkv@hotmail.com>
2014-08-27 22:42:36 +0100

    Fixing listbox height issue

--- BasicGitClient/ClientMainWindow.Designer.cs
+++ BasicGitClient/ClientMainWindow.Designer.cs
@@ -370,6 +370,7 @@
             this.listBox1.ContextMenuStrip = this.cMnuFileViewer;
             this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.IntegralHeight = false;
             this.listBox1.Location = new System.Drawing.Point(0, 0);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(197, 184);
@@ -403,7 +404,7 @@
             this.renameFileToolStripMenuItem,
             this.deleteToolStripMenuItem});
             this.cMnuFileViewer.Name = "cMnuFileViewer";
-            this.cMnuFileViewer.Size = new System.Drawing.Size(153, 114);
+            this.cMnuFileViewer.Size = new System.Drawing.Size(144, 92);
             // 
             // newFileToolStripMenuItem
             // 

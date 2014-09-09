5ea52ef0feaa976fc4061b2eff2f42bb9a8ce935
RedSpiderMkV <redspidermkv@hotmail.com>
2014-09-09 22:18:17 +0100

    Checking rename of repo url worked fine

--- BasicGitClient/GitCommands.cs
+++ BasicGitClient/GitCommands.cs
@@ -11,18 +11,18 @@ namespace BasicGitClient
     /// </summary>
     public class GitCommands
     {
-        public const string STATUS = "status";
-        public const string INIT = "init";
-        public const string PULL = "pull";
-        public const string COMMIT = "commit -m";
         public const string ADD = "add";
         public const string ADD_ALL = "add -A";
         public const string CLONE = "clone ";
-        public const string SHOW_ORIGIN = "remote -v";
+        public const string COMMIT = "commit -m";
+        public const string INIT = "init";
+        public const string PULL = "pull";
         public const string SET_ORIGIN = "remote add origin ";
-        public const string RESET = "reset --hard";
         public const string SET_EMAIL = "config --global user.email ";
         public const string SET_USERNAME = "config --global user.name ";
+        public const string SHOW_ORIGIN = "remote -v";
+        public const string STATUS = "status";
+        public const string RESET = "reset --hard";
         public const string REVERT = "revert HEAD";
 
         public static string PUSH = "push https://{0}:{1}@github.com{2} master";

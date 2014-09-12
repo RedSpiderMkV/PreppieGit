20875d126f240a8ef429127c2179d9ff9a68d4ee
RedSpiderMkV <redspidermkv@hotmail.com>
2014-09-12 21:59:59 +0100

    Correcting bug where read-only/hidden files were causing errors during delete

--- BasicGitClient/ClientMainWindow.cs
+++ BasicGitClient/ClientMainWindow.cs
@@ -432,10 +432,53 @@ namespace BasicGitClient
             tvDirectoryList_NodeMouseClick(this, null);
         }
 
+        private void DeleteDirectory(string path, bool recursive)
+        {
+            // Delete all files and sub-folders?
+            if (recursive)
+            {
+                // Yep... Let's do this
+                var subfolders = Directory.GetDirectories(path);
+                foreach (var s in subfolders)
+                {
+                    DeleteDirectory(s, recursive);
+                }
+            }
+
+            // Get all files of the folder
+            var files = Directory.GetFiles(path);
+            foreach (var f in files)
+            {
+                // Get the attributes of the file
+                var attr = File.GetAttributes(f);
+
+                // Is this file marked as 'read-only'?
+                if ((attr & FileAttributes.ReadOnly) == FileAttributes.ReadOnly)
+                {
+                    // Yes... Remove the 'read-only' attribute, then
+                    File.SetAttributes(f, attr ^ FileAttributes.ReadOnly);
+                }
+
+                // Delete the file
+                File.Delete(f);
+            }
+
+            // When we get here, all the files of the folder were
+            // already deleted, so we just delete the empty folder
+            Directory.Delete(path);
+        }
+
         private void deleteFolderToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Directory.Delete(treeViewSelectedDirectory, true);
-            populateTreeView();
+            try
+            {
+                DeleteDirectory(treeViewSelectedDirectory, true);
+                populateTreeView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private bool lbFileListSelectedCheck()

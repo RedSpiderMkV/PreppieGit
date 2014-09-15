ccbf4f19fb4491ca0a8df70b07a758faed9a149b
RedSpiderMkV <redspidermkv@hotmail.com>
2014-09-15 23:29:52 +0100

    Renaming assembly

--- BasicGitClient/Properties/Resources.Designer.cs
+++ BasicGitClient/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BasicGitClient.Properties {
+namespace PreppieGit.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BasicGitClient.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BasicGitClient.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PreppieGit.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- BasicGitClient/Properties/Settings.Designer.cs
+++ BasicGitClient/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BasicGitClient.Properties {
+namespace PreppieGit.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]

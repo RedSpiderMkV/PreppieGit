16f6c1556a717fc5be71054f1e7b65ac097fa287
RedSpiderMkV <redspidermkv@hotmail.com>
2015-08-19 21:13:26 +0100

    button layoput update

--- src/BasicGitClient/UI/Components/ButtonGroup.cs
+++ src/BasicGitClient/UI/Components/ButtonGroup.cs
@@ -33,18 +33,6 @@ namespace BasicGitClient
             repoName_m = newRepoName;
         } // end method
 
-        private void btnScroll_Click(object sender, EventArgs e)
-        {
-            if (sender == btnScrollLeft)
-            {
-                panelInner.Location = new Point(panelInner.Location.X + 10, panelInner.Location.Y);
-            }
-            else if (sender == btnScrollRight)
-            {
-                panelInner.Location = new Point(panelInner.Location.X - 10, panelInner.Location.Y);
-            } // end if
-        } // end method
-
         private void btn_Click(object sender, EventArgs e)
         {
             if (sender == btnAdd)

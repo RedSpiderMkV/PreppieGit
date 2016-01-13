39b783a6b1128d0e385619d8a60083b77323e774
redspidermkv <redspidermkv@hotmail.com>
2016-01-13 23:06:14 +0000

    adding status logging interface in prep for a status logger

---
+++ src/BasicGitClient/StatusLogger/IStatusLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StatusLogger
+{
+    interface IStatusLogger
+    {
+        void LogStatus(string statusMessage);
+
+        void LogException(Exception exception);
+    } // end interface
+} // end namespace
